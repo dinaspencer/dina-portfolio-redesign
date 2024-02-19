4fc2a23dda7b96ca85b287c02812c4f54370bce3
Dina Spencer <dkay22@gmail.com>
2024-02-19 10:34:41 +0100

    Update profile images and colors

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ export default function About () {
                 <h1>who am I?</h1>
                 <h5><MdLocationPin />  Zaragoza, Spain</h5>
                 <h5><MdHome /> Winston-Salem, North Carolina</h5>
-                <p>I decided to leave the US and have lived in Spain for over ten years. In Spain I've worked as an auxiliar de conversación in high schools, a community manager in a startup, a digital editor, and a business English communication coach. After much experience navigating the Spanish visa system, I'm now Autónoma with long-term residency.</p>
+                <p>I decided to leave corporate America in 2013 and have lived in Spain for over ten years. In Spain I've worked as an auxiliar de conversación in high schools, a community manager in a startup, a digital editor, and a business English communication coach. After much experience navigating the Spanish visa system, I'm now Autónoma with long-term residency.</p>
                 <p>With a psychology degree and years of teaching experience under my belt, I coach and support business English learners to speak more fluently and feel more confident. I also create speaking games using the React framework to practice coding on a real-world project.</p>
                 <p>Hobbies: personal finance and budgeting, photography, singing in choirs, running, cycling, writing, reading, and learning to code.</p>  
             </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ export default function Main () {
             <h1>hi, I'm Dina.</h1>
                 <p>I help people and companies express themselves better.</p>
                 <p>With over 10 years of experience in language coaching, web design and writing, I can polish your speaking, writing, and your online presence.</p>
-                <div className="profile-round"></div>
+                <div className="profile-round" style={{backgroundImage:"url(/img/profile.jpg)",  backgroundSize: "contain"}}></div>
                 
             </div>
         </section>
