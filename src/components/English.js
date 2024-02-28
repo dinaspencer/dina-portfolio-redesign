9af108cf03b65c88664cf370900a214ccb79eb8f
Dina Spencer <dkay22@gmail.com>
2024-02-28 18:51:21 +0100

    netlify tweaks

--- src/components/English.js
+++ src/components/English.js
@@ -11,7 +11,7 @@ export default function English () {
                         <div className="pdf-signup">
         <h4><strong>Do you want to speak English with better fluency?</strong></h4> 
         <h4><strong>Download my free PDF guide!</strong></h4>
-        <iframe title="pdf-signup" width="90%" height="505" src="https://0760bbe7.sibforms.com/serve/MUIFAEAGX5GJzBtXKj0i9KpfZTmY1vegkUpizxm-9AYLOrNRGWaCY7tpe4KbVHU8Vj7gBhgIyzFmf-K-2XPuINcRP3SRTI2clk_GVrpAy-77h1Ujmszmovsh23S2ewPqq1hRm-Dm3NTcbKIjXnNkRclOMkaNAVC8IOKRyyBRxKvsktY2szmwRROkeU87Y25iacqXyiVZ_QQ5XRAN"  scrolling="auto" allowFullScreen ></iframe>
+        <iframe title="pdf-signup" width="100%" height="505" src="https://0760bbe7.sibforms.com/serve/MUIFAEAGX5GJzBtXKj0i9KpfZTmY1vegkUpizxm-9AYLOrNRGWaCY7tpe4KbVHU8Vj7gBhgIyzFmf-K-2XPuINcRP3SRTI2clk_GVrpAy-77h1Ujmszmovsh23S2ewPqq1hRm-Dm3NTcbKIjXnNkRclOMkaNAVC8IOKRyyBRxKvsktY2szmwRROkeU87Y25iacqXyiVZ_QQ5XRAN"  scrolling="auto" allowFullScreen ></iframe>
                         </div>
                 </div>
         
