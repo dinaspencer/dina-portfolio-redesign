1f37124b1ff480301989fecfcaa32e4a90621534
Dina Spencer <dkay22@gmail.com>
2024-02-28 19:08:11 +0100

    remove mailchimp

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,7 +7,6 @@ import { AiOutlineArrowDown } from "react-icons/ai";
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-// import MailchimpFormContainer from "./MailChimpForm";
 
 export default function Services () {
 
@@ -41,8 +40,7 @@ export default function Services () {
                 <Offcanvas.Header closeButton>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                    {/* <MailchimpFormContainer /> */}
-                    {/* <iframe id="services-signup" src="https://0760bbe7.sibforms.com/serve/MUIFAHpNi48laHpCItUT63_H4NfbHd1wYm_OIantG-ifCsBKe08uWFguis9tMucyT5OD-ekrdqpyiipfg8CGgENcseCQqc8LUUgiGXob2Ym7yYqsy4TkVIfy5fMz09o1GgwK4xtqA6ujt-921sR9J0hKnRTHdXgnmeVznlpmB-66zynXu4eC55lgL9tUN5EnZjDnJhDimstQhYQy"  scrolling="auto" title="Subscribe" allowFullScreen height="500" styles={{padding: 12}}  ></iframe> */}
+                   
 
                     <iframe title="newsletter" id="services-signup" src="https://0760bbe7.sibforms.com/serve/MUIFAJopn2TPn1zjyzZYtw6fQIGFjijfTrZ_exkL-_Xk79WDn8BIBF7kMeRNBMYs7d1bRU68nqMriHm9WmCze3mapCn2pm7KSHsmZ0IE_t2d0FgWZXkxKKk-yf5Svi6M70pksbevI2526-fsJT5BiE71Bcko9z_E9hV8HFgHserroMKs2zKFMG_3jn41QBVcu19BZ03ZG-5MMGC4"  scrolling="auto" allowFullScreen ></iframe>
                 </Offcanvas.Body>
