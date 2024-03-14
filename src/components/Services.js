e7ae052a5fb269851ff3b12f30f431df290a58ff
Dina Spencer <dkay22@gmail.com>
2024-03-14 09:48:11 +0100

    add flip to circles

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,10 +3,43 @@ import { SiAbbott } from "react-icons/si";
 import { SiLinkedin } from "react-icons/si";
 import { SiTwitter } from "react-icons/si";
 import { SiFacebook } from "react-icons/si";
-// import { AiOutlineArrowDown } from "react-icons/ai";
+import { AiOutlineArrowRight } from "react-icons/ai";
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+
+
+const content = [
+    {"id": 1, "front": "strengthen communication skills", "back": "so you can be recognized as a candidate for better positions"},  
+    {"id": 2, "front": "speak confidently in meetings", "back": "so you can be respected as a strong leader"}, 
+    {"id": 3, "front": "give outstanding presentations", "back": "so you can gain the trust of stakeholders"}, 
+    {"id": 4, "front": "improve fluency", "back": "so you can be yourself and connect with others"},
+      ]
+
+      const shuffleCards = content.sort(() => Math.random() - 0.5);
+  
+const ServicesCircle = ({circle}) => {
+    const [flip, setFlip] = useState(false);
+    return (
+    <div>
+        <li className= {`services-circle ${flip ? 'flip' : ''}`} onClick={() => setFlip(!flip)}>
+            <p className="front">{circle.front} <AiOutlineArrowRight/></p>
+            <p className="back">{circle.back}</p>
+        </li>      
+    </div>
+    )
+}
+
+const ServicesList = ({circles}) => {
+    return (
+        <ul>
+            {circles.map(circle => {
+                return <ServicesCircle circle={circle} key={circle.id} />
+            })}
+        </ul>
+    )
+   
+}
 
 export default function Services () {
 
@@ -15,21 +48,25 @@ export default function Services () {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+   
+   const [circles] = useState(shuffleCards);
 
     return (
         <>
         <section id="services">
             <div className="container-main">
         <h3>english coaching for intermediate and advanced learners</h3>
+        <h3>who want to...</h3>
         
         
-        <ul>
+       
+                <ServicesList circles={circles} />
             
-            <li className="services-circle"><p>develop leadership skills</p></li>
+            {/* <li className= {`services-circle ${flip ? 'flip' : ''}`} onClick={() => setFlip(!flip)}><p>develop leadership skills</p></li>
             <li className="services-circle"><p>feel confident in meetings</p></li>
             <li className="services-circle"><p>give outstanding presentations</p></li>
-            <li className="services-circle"><p>improve your fluency</p></li>
-        </ul>
+            <li className="services-circle"><p>improve your fluency</p></li> */}
+      
 
             <div className="english-buttons">
             {/* <Button  className="service-btn" href="#contact" >Work with me</Button> */}
