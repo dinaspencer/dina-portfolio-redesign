cb72fa1b67dcf8e71114fd1d407d42ef9174e877
Dina Spencer <46998017+dinaspencer@users.noreply.github.com>
2025-02-04 10:57:36 +0100

    new branch commit

--- src/components/About.js
+++ src/components/About.js
@@ -14,8 +14,8 @@ export default function About () {
                 <h5><MdLocationPin />  Zaragoza, Spain</h5>
                 <h5 className="about-header"><MdHome /> Winston-Salem, North Carolina</h5>
                 <p>I decided to leave corporate America in 2013 and have lived in Spain for over ten years. In Spain I've <a href="#cv"> worked</a> as an auxiliar de conversación in high schools, a community manager in a startup, a digital editor, and a business English communication coach. After much experience navigating the Spanish visa system, I'm now Autónoma with long-term residency.</p>
-                <p>With a psychology degree and years of teaching experience under my belt, I coach and support business English learners to speak more fluently and feel more confident. I also create speaking games using the React framework to practice coding on a real-world project.</p>
-                <p>Hobbies: personal finance and budgeting, <a href="https://dinaspencer.portfoliobox.io" target="_blank" rel="noreferrer">photography</a>, singing in choirs, running, cycling, writing, reading, and learning to code.</p>  
+                <p>With a psychology degree and several years of teaching experience, I coach and support business English learners to speak more fluently and feel more confident. I also create <a href="https://englishcards.netlify.app/" target="_blank" rel="noreferrer">speaking activities</a> using the React framework to practice coding on a scalable real-world project. I'm currently in business school, so I don't have time for very much else!</p>
+                <p>Hobbies: personal finance and budgeting, <a href="https://dinaspencer.portfoliobox.io" target="_blank" rel="noreferrer">photography</a>, singing in choirs, writing, reading, and discovering different parts of Spain.</p>  
             </div>
             <div className="image-half-page">
             <img src="/img/spainmap.png" alt="spain-map" className="background-map"/>
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,9 @@ export default function Main () {
         <section id="main">
             <div className="container-main">
             <h1>hi, I'm Dina.</h1>
-                <p>I help people and companies express themselves better.</p>
-                <p>With over 10 years of experience in language coaching, web design and writing, I can polish your speaking, writing, and your online presence.</p>
+               
+                <p>I've lived in Spain for over 10 years as both auxiliar de conversación and autónoma. </p>
+                <p>Need advice about navigating life in Spain? I'm here to help!</p>
                 <div className="profile-round" style={{backgroundImage:"url(/img/photomain.jpg)",  backgroundSize: "contain"}}></div>
                 
             </div>
