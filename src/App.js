6df964f69c7dd7b5044cf18e0d1a58776c4399de
Dina Spencer <46998017+dinaspencer@users.noreply.github.com>
2025-02-04 10:25:28 +0100

    first commit, project copied from redesign

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
 import About from './components/About';
-import Services from './components/Services';
+// import Services from './components/Services';
 import English from './components/English';
-import Cv from './components/Cv';
+// import Cv from './components/Cv';
 import Blog from './components/Blog';
-import Dev from './components/Dev';
+// import Dev from './components/Dev';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -18,12 +18,12 @@ function App() {
     <div className="App">
      <NavBar />
       <Main />
-      <Services />
+      {/* <Services /> */}
       <English />
-      <Dev />
+      {/* <Dev /> */}
       <About />
       <Contact />
-      <Cv />
+      {/* <Cv /> */}
       <Blog />
       <Footer />
     </div>
